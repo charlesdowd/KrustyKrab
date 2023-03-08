083f2a9147726ab406c1d259987aa670b7196e42
Charles Dowd <charlie.dowd96@gmail.com>
2023-03-08 12:11:46 -0500

    handle empty mongouri error

--- server/src/config/dbConnection.ts
+++ server/src/config/dbConnection.ts
@@ -1,14 +1,13 @@
 import mongoose from 'mongoose';
 
-// Set a default in case env variable is undefined
-const { DATABASE_URI = '' } = process.env;
-
 const connectDB = async () => {
   try {
     // Gets rid of warning of some mongoose deprecation.
     mongoose.set('strictQuery', false);
 
-    mongoose.connect(DATABASE_URI);
+    if (!process.env.DATABASE_URI) throw new Error('Database uri is empty');
+
+    mongoose.connect(process.env.DATABASE_URI);
   } catch (error) {
     console.log(error);
   }
